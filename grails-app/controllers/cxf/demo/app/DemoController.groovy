3259ed1b7dbf60c3d4fb0b18880d833ba03a098c
coestre <christian.oestreich@optum.com>
2017-03-06 20:23:38 -0600

    Initial Commit with all classes

--- grails-app/controllers/cxf/demo/app/DemoController.groovy
+++ grails-app/controllers/cxf/demo/app/DemoController.groovy
@@ -1,19 +1,32 @@
 package cxf.demo.app
 
+import com.cxf.demo.sample.client.SampleService
 import net.webservicex.ICD9Soap
 
 class DemoController {
 
     ICD9Soap icd9Client
+    SampleService localClient
 
     def index() {
         render(view: "/index")
     }
 
     def icd9() {
-        String data = ""
+        String data
         try {
             data = icd9Client.getICD9ByDescription("Neoplasms")
+        } catch (Exception e) {
+            data = e.message
+        }
+
+        render(view: '/index', model: [data: data])
+    }
+
+    def local() {
+        String data
+        try {
+            data = localClient.serviceMethod()
         } catch (Exception e) {
             data = e.message
         }
